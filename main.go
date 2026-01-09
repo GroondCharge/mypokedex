8ce01e92fc5e01c961c1e32baf9cdcc4f965103c
janz <jan.zalar6@gmail.com>
2026-01-09 13:30:56 +0100

    Revert "tui implementation"
    This reverts commit e0da55266511ef2c4b85853e8118b93f0a120939.
    
    reverting tui

--- main.go
+++ main.go
@@ -1,28 +1,14 @@
 package main
 
 import (
-	"fmt"
-	"os"
 	"pokedex/internal/pokeapi"
-	"pokedex/tui"
 	"time"
-
-	tea "github.com/charmbracelet/bubbletea"
 )
 
 func main() {
 	pokeClient := pokeapi.NewClient(5*time.Second, 60*time.Second)
-
-	// Create TUI config
-	cfg := &tui.Config{
-		PokeapiClient: pokeClient,
-		Inventory:     make(map[string]pokeapi.PokemonStruct),
+	cfg := &Config{
+		pokeapiClient: pokeClient,
 	}
-
-	// Start Bubble Tea TUI
-	p := tea.NewProgram(tui.New(cfg), tea.WithAltScreen())
-	if _, err := p.Run(); err != nil {
-		fmt.Printf("Error running TUI: %v\n", err)
-		os.Exit(1)
-	}
+	startRepl(cfg)
 }
